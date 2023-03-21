25c80b75e24ced85b1836a6c693e94c33140d36f
Suntra <47025714+dffxd-suntra@users.noreply.github.com>
2023-03-21 22:43:29 +0800

    Update nyahentai-plus.user.js

--- nyahentai-plus.user.js
+++ nyahentai-plus.user.js
@@ -6,12 +6,11 @@
 // @homepageURL  https://github.com/dffxd-suntra/nyahentai-plus
 // @supportURL   https://github.com/dffxd-suntra/nyahentai-plus
 // @match        https://nyahentai.red/g/*
+// @match        https://nhentai.net/g/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=nyahentai.red
 // @require      https://lf6-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.6.0/jquery.min.js
 // @author       Suntra
-// @run-at       document-end
 // @license      MIT
-// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
@@ -19,19 +18,18 @@
     function startView() {
         if(inited) return;
         inited = true;
-        var pages = $("#tags > div:nth-child(7) > span > a > span").text();
+        var pages = new Number($("#tags").children(":contains('Pages')").find(".tag").text());
         var tempUrl = $("#cover > a > img").attr("src").match(/https:\/\/cdn.nhentai.xxx\/g\/[0-9]+\//);
         console.log("pages: "+pages);
         console.log("tempUrl: "+tempUrl);
 
         for(var i=1;i<=pages;i++) {
-            $("#nyap-read-page-img").append(`<img src="`+tempUrl+`/`+i+`.jpg" class="lazyload" style="width: 100%"/><br>`);
+            $("#nyap-read-page-img").append(`<img src="`+tempUrl+i+`.jpg" class="lazyload" style="width: 100%"/><br>`);
         }
     }
-
-    GM_addStyle(`.hide {display: none !important;}`);
+    // 页面样式
     $("body").prepend(`
-    <div style="width: 100%;display: none;" id="nyap-read-page">
+    <div style="position: fixed;top: 0;left: 0;right: 0;bottom: 0;z-index: 9999;display: none;background: rgba(0, 0, 0, 95%);overflow: auto;" id="nyap-read-page">
         <div style="position: fixed;bottom: 10px;right: 10px;">
             <h1 id="showWidth">80%</h1>
             <div id="nyap-read-page-img-change-width">
@@ -43,7 +41,7 @@
                 <button type="button" class="btn btn-primary">-1</button><br>
             </div>
             <button type="button" id="nyap-read-page-hide"  class="btn btn-primary">关闭</button><br>
-            <button type="button" onclick="javascript:document.body.scrollIntoView({block: 'start'});" class="btn btn-primary">顶部</button>
+            <button type="button" id="nyap-read-page-toTop" class="btn btn-primary">顶部</button>
         </div>
         <center>
         <div id="nyap-read-page-img" style="width: 80%">
@@ -52,29 +50,20 @@
     </div>
     `);
     $("#info > div").prepend(`<button class="btn btn-primary" id="nyap-read-page-show">垂直阅读</button>`);
+    // 切换开,关
     $("#nyap-read-page-show").click(function () {
-        $("body").children().each(function (i,e) {
-            console.log(e);
-            if($(e).attr("id")=="nyap-read-page") {
-                $(e).css("display","");
-            }
-            else {
-                $(e).addClass("hide");
-            }
-        });
         startView();
+        $("body").css("overflow", "hidden");
+        $("#nyap-read-page").show();
     });
     $("#nyap-read-page-hide").click(function () {
-        $("body").children().each(function (i,e) {
-            console.log(e);
-            if($(e).attr("id")=="nyap-read-page") {
-                $(e).css("display","none");
-            }
-            else {
-                $(e).removeClass("hide");
-            }
-        });
+        $("body").css("overflow", "");
+        $("#nyap-read-page").hide();
     });
+    $("#nyap-read-page-toTop").click(function () {
+        $("#nyap-read-page").scrollTop(0);
+    });
+    // 切换宽度
     var imgWidth = 80;
     $("#nyap-read-page-img-change-width").click(function ({target}) {
         if($(target).attr("id")=="nyap-read-page-img-change-width") {
